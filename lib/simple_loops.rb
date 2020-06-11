5d89e75e36ee5cb4ca4170d08105a87048b32370
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-11 17:50:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,10 +18,12 @@ def output_array (array)
   end
 end
 
-def return_string_array
+def return_string_array(array)
   count = 0
+  new_array = []
   while count < array.length do
-     array[count].to_s
-    count +=1
+    new_array.push(array[count].to_s)
+    count += 1
   end
+  new_array
 end
