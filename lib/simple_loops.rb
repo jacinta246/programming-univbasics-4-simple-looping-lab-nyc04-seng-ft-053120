34ff59ac8a32c137246c069cac310c24687814a9
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-11 17:42:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+def loop_message_five_times (message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times (message, integer)
+  integer.times do
+    puts message
+  end
+end
+
+def output_array (array)
+  count = 0
+  while count < array.length do
+    puts array [count]
+    count +=1
+  end
+end
